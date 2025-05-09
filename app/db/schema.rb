30b5717093fabe7c48df0eb2782d91bd150cb5d8
ogatamegumi <mmmegumi.33@gmail.com>
2025-05-09 16:04:33 +0900

    Tasksテーブルを追加

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_09_062603) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_09_064956) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "category"
+    t.string "frequency"
+    t.text "message"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
