2b07bad53dcd5f28b396fe6b33f8fed39b7215c5
ogatamegumi <mmmegumi.33@gmail.com>
2025-05-18 18:12:23 +0900

    マイページ準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users
+  resources :users, only: [:show]
 
   resources :tasks do
     resources :task_logs, only: [ :create ]
