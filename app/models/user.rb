c2a9d8901e83de155284ce0cbc692abbaffd2e2d
ogatamegumi <mmmegumi.33@gmail.com>
2025-06-01 17:21:35 +0900

    ユーザー名を設定する機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  before_validation :set_default_name, if: -> { name.blank? && email.present? }
+
+  validates :name, presence: true
+
+  private
+
+  def set_default_name
+    self.name = email.split("@").first
+  end
 end
